ce582333efd4fa79833bd47f56cb3b33e37e3a18
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-08 04:24:05 +0300

    Refactor contact component to add template reference variable and ViewChild examples; enhance functionality for updating displayed names

--- src/app/component/contact/contact.ts
+++ src/app/component/contact/contact.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren} from '@angular/core';
 import {FormsModule} from '@angular/forms'
+import { ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-contact',
   imports: [FormsModule],
@@ -13,6 +14,22 @@ export class Contact {
     age : 20,
     address: '123 Main St, Anytown, USA'
   }
-  name:string="";
+  name:string="amr";
+  @ViewChildren('nameDisplay') nameDisplay!:  QueryList<ElementRef>;
+  @ViewChild('paragraph')paragraphele!:ElementRef;
+
+printTemplateRefVar(templateRefVar:HTMLParagraphElement) {
+  console.log(templateRefVar.textContent);
 }
+  printParagraph() {
+    console.log(this.paragraphele.nativeElement.textContent);
+  }
+  handleName(nameInput: HTMLInputElement) {
+    this.nameDisplay.forEach((element: ElementRef) => {
+      element.nativeElement.textContent = nameInput.value;
+    });
+  }
+
+}
+
 
